e127bb9598a723a2d3c7e275685683a6cc41665b
KhrystynaKh <christinehorynjak@gmail.com>
2022-05-30 19:28:25 +0300

    Made changes about my code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-alert("Hello World");
+// current day and time
+let date = new Date();
+let week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = week[date.getDay()];
+let hours = date.getHours();
+let minutes = date.getMinutes();
+
+if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+
+let time = (document.querySelector(
+  "#time"
+).innerHTML = `${day} ${hours}:${minutes}`);
